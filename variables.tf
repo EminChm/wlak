c5ef2e918b0bc70a531e12f071cf258d0199986f
EminChm <emin.chumarin@gmail.com>
2023-03-21 17:48:23 +0400

    push hw

---
+++ variables.tf
@@ -0,0 +1,44 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+  default     = "y0_AgAAAAALozx_AATuwQAAAADaf986ZH2iWxf_RomQi0nXPYiKSMdMNzQ"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  default     = "b1grn1e3krsr60esrsqh"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  default     = "b1g8mfirhctpcoq457fr"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "NETTER"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "<ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEgP+vC44t4sOFQFBC99TQ77xy4/iWQXNgSp3yjieEQ emin2023@deb1>"
+  description = "ssh-keygen -t ed25519"
+}
